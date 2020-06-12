06ebc6409fb7818c5a8a34c5f3f1a6ef0a6edf83
dbabbs <dylan.babbs@gmail.com>
2020-06-12 15:39:14 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mobility-map/precache-manifest.6546636583adfb96255cd3c506acc305.js"
+  "/mobility-map/precache-manifest.154f3afa1d6ce9cfcbc1b89b5feac964.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mobility-map/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
